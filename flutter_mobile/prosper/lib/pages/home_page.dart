d58ccb28ce10e4354a5e4576b7e5f22fc024c3ea
Ashok <ashok_a380@icloud.com>
2020-01-14 09:03:48 +0530

    git commit

--- flutter_mobile/prosper/lib/pages/home_page.dart
+++ flutter_mobile/prosper/lib/pages/home_page.dart
@@ -1,42 +1,174 @@
 import 'package:flutter/material.dart';
-
+import 'package:prosper/utils/card_item.dart';
 class HomePage extends StatefulWidget {
   @override
-  _HomePageState createState() => _HomePageState();
+  _HomePageState createState() => new _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin{
+
+  var appColors = [Color.fromRGBO(231, 129, 109, 1.0),Color.fromRGBO(99, 138, 223, 1.0),Color.fromRGBO(111, 194, 173, 1.0)];
+  var cardIndex = 0;
+  ScrollController scrollController;
+  var currentColor = Color.fromRGBO(231, 129, 109, 1.0);
+
+  var cardsList = [CardItemModel("Personal", Icons.account_circle, 9, 0.83),CardItemModel("Work", Icons.work, 12, 0.24),CardItemModel("Home", Icons.home, 7, 0.32)];
+
+  AnimationController animationController;
+  ColorTween colorTween;
+  CurvedAnimation curvedAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    scrollController = new ScrollController();
+
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Color(0xFF1B2430),
-        body: SafeArea(
-            child: Container(
-          child: Column(
-            children: <Widget>[
-              SizedBox(
-                height: 25,
-              ),
-              Align(
-                alignment: Alignment.topLeft,
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 20.0),
-                  child: RichText(
-                    text: TextSpan(
-                      children: [
-                        TextSpan( text: 'Hello,',
-                            style: TextStyle(
-                                fontSize: 32,
-                                fontWeight: FontWeight.w700,
-                                color: Color(0xFF5DBCD2))),
-                      ],
+    return new Scaffold(
+      backgroundColor: currentColor,
+      appBar: new AppBar(
+        title: new Text("TODO", style: TextStyle(fontSize: 16.0),),
+        backgroundColor: currentColor,
+        centerTitle: true,
+        actions: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(right: 16.0),
+            child: Icon(Icons.search),
+          ),
+        ],
+        elevation: 0.0,
+      ),
+      body: new Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Row(),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 64.0, vertical: 32.0),
+              child: Container(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 16.0),
+                      child: Icon(Icons.account_circle, size: 45.0, color: Colors.white,),
                     ),
-                  ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0.0,16.0,0.0,12.0),
+                      child: Text("Hello.", style: TextStyle(fontSize: 30.0, color: Colors.white, fontWeight: FontWeight.w400),),
+                    ),
+                    Text("Looks like feel good.", style: TextStyle(color: Colors.white),),
+                  ],
                 ),
               ),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 64.0, vertical: 16.0),
+                  child: Text("TODAY : JUL 21, 2018", style: TextStyle(color: Colors.white),),
+                ),
+                Container(
+                  height: 350.0,
+                  child: ListView.builder(
+                    physics: NeverScrollableScrollPhysics(),
+                    itemCount: 3,
+                    controller: scrollController,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, position) {
+                      return GestureDetector(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Card(
+                            child: Container(
+                              width: 250.0,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: <Widget>[
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Row(
+                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                      children: <Widget>[
+                                        Icon(cardsList[position].icon, color: appColors[position],),
+                                        Icon(Icons.more_vert, color: Colors.grey,),
+                                      ],
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: <Widget>[
+                                        Padding(
+                                          padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+                                          child: Text("${cardsList[position].tasksRemaining} Tasks", style: TextStyle(color: Colors.grey),),
+                                        ),
+                                        Padding(
+                                          padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+                                          child: Text("${cardsList[position].cardTitle}", style: TextStyle(fontSize: 28.0),),
+                                        ),
+                                        Padding(
+                                          padding: const EdgeInsets.all(8.0),
+                                          child: LinearProgressIndicator(value: cardsList[position].taskCompletion,),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10.0)
+                            ),
+                          ),
+                        ),
+                        onHorizontalDragEnd: (details) {
 
-            ],
-          ),
-        )));
+                          animationController = AnimationController(vsync: this, duration: Duration(milliseconds: 500));
+                          curvedAnimation = CurvedAnimation(parent: animationController, curve: Curves.fastOutSlowIn);
+                          animationController.addListener(() {
+                            setState(() {
+                              currentColor = colorTween.evaluate(curvedAnimation);
+                            });
+                          });
+
+                          if(details.velocity.pixelsPerSecond.dx > 0) {
+                            if(cardIndex>0) {
+                              cardIndex--;
+                              colorTween = ColorTween(begin:currentColor,end:appColors[cardIndex]);
+                            }
+                          }else {
+                            if(cardIndex<2) {
+                              cardIndex++;
+                              colorTween = ColorTween(begin: currentColor,
+                                  end: appColors[cardIndex]);
+                            }
+                          }
+                          setState(() {
+                            scrollController.animateTo((cardIndex)*256.0, duration: Duration(milliseconds: 500), curve: Curves.fastOutSlowIn);
+                          });
+
+                          colorTween.animate(curvedAnimation);
+
+                          animationController.forward( );
+
+                        },
+                      );
+                    },
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+      drawer: Drawer(),
+    );
   }
 }
